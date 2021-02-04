499d63851f8ceba95b5ac7f73e9fa45ccc6e6190
AugustoQueiroz <asq@cin.ufpe.br>
2021-02-04 13:17:37 -0300

    Print the possible solutions

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <tuple>
 
 #include <omp.h>
 
@@ -33,34 +34,43 @@ int main() {
 
     std::vector<Edge*> cubeEdges = cubes->getEdges();
 
-    for (int i = 0; i < cubeEdges.size(); i++) {
-        std::cout << cubeEdges[i]->getCube() << " " << cubeEdges[i]->getTerminal1() << " " << cubeEdges[i]->getTerminal2() << std::endl;
-    }
-
     std::vector<Graph> Hs = cubes->getHs();
 
-    std::cout << Hs.size() << std::endl;
+    std::cout << "There are " << Hs.size() << " candidates for H1 & H2." << std::endl;
 
-    for (int i = 0; i < Hs.size(); i++) {
-        std::vector<Edge*> edges = Hs[i].getEdges();
-        for (int j = 0; j < edges.size(); j++) {
-            std::cout << edges[j]->getCube() << " " << edges[j]->getTerminal1() << " " << edges[j]->getTerminal2() << std::endl;
-        }
-        std::cout << std::endl;
-    }
-
-    std::cout << "------" << std::endl;
-
-    int solutionCounter = 0;
-    #pragma omp parallel for reduction(+:solutionCounter)
+    std::vector<std::tuple<Graph, Graph>> solutions;
+    // #pragma omp declare reduction (merge : std::vector<std::tuple<Graph, Graph>> : omp_out.insert(omp_out.end(), omp_in.begin(), omp_in.end()))
+    // #pragma omp parallel for reduction(merge: solutions)
     for (int i = 0; i < Hs.size(); i++) {
         Graph H1 = Hs[i];
         std::vector<Graph> validH2s;
         std::copy_if(Hs.begin()+i, Hs.end(), std::back_inserter(validH2s), [&, H1](Graph g){ return g.edgesSharedWith(H1).size() == 0; });
-        solutionCounter += validH2s.size();
+
+        for (int j = 0; j < validH2s.size(); j++) {
+            solutions.push_back(std::make_tuple(H1, validH2s[j]));
+        }
     }
 
-    std::cout << "There are " << solutionCounter << " possible solutions." << std::endl;
+    std::cout << "There are " << solutions.size() << " possible solutions." << std::endl;
+
+    for (int i = 0; i < solutions.size(); i++) {
+        std::cout << "Solution " << i << std::endl;
+
+        Graph H1 = std::get<0>(solutions[i]);
+        std::vector<Edge*> H1Edges = H1.getEdges();
+        Graph H2 = std::get<1>(solutions[i]);
+        std::vector<Edge*> H2Edges = H2.getEdges();
+
+        std::cout << "Front & Back:" << std::endl;
+        for (int j = 0; j < H1Edges.size(); j++) {
+            std::cout << H1Edges[j]->getCube() << " " << H1Edges[j]->getTerminal1() << " " << H1Edges[j]->getTerminal2() << std::endl;
+        }
+
+        std::cout << "Left & Right:" << std::endl;
+        for (int j = 0; j < H2Edges.size(); j++) {
+            std::cout << H2Edges[j]->getCube() << " " << H2Edges[j]->getTerminal1() << " " << H2Edges[j]->getTerminal2() << std::endl;
+        }
+    }
 
     return 0;
 }
